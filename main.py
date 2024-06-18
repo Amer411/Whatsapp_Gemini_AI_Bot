5708ef40f1473bbfec335585d333586beb4fb18c
Amer411 <155028715+Amer411@users.noreply.github.com>
2024-06-18 04:35:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import fitz
 wa_token = os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
 phone_id = os.environ.get("PHONE_ID")
-bot_name = "عمرو"  # This will be the name of your bot, eg: "Hello I am Astro Bot"
-model_name = "gemini-1.5-flash-latest"  # Switch to "gemini-1.0-pro" or any free model, if "gemini-1.5-flash" becomes paid in future.
+bot_name = "عمرو"
+model_name = "gemini-1.5-flash-latest"
 
 app = Flask(__name__)
 
@@ -30,7 +30,6 @@ model = genai.GenerativeModel(model_name=model_name,
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-# Store conversation state for each user
 conversations = {}
 
 def send(phone, answer):
@@ -96,7 +95,9 @@ def webhook():
                         pix = page.get_pixmap()
                         pix.save(destination)
                         file = genai.upload_file(path=destination, display_name="tempfile")
-                        response = model.generate_content(["اشرح محتوى الصوره", file])
+                        comment = data.get("caption", "")
+                        prompt = f"اشرح محتوى الصورة مع التعليق: {comment}"
+                        response = model.generate_content([prompt, file])
                         answer = response._result.candidates[0].content.parts[0].text
                         convo.send_message(f"This message is created by an llm model based on the image prompt of user, reply to the user based on this: {answer}")
                         send(phone, convo.last.text)
@@ -107,7 +108,9 @@ def webhook():
                 with open(filename, "wb") as temp_media:
                     temp_media.write(media_download_response.content)
                 file = genai.upload_file(path=filename, display_name="tempfile")
-                response = model.generate_content(["ماهذا؟", file])
+                comment = data.get("caption", "")
+                prompt = f"ماهذا؟ {comment}"
+                response = model.generate_content([prompt, file])
                 answer = response._result.candidates[0].content.parts[0].text
                 remove("/tmp/temp_image.jpg", "/tmp/temp_audio.mp3")
                 convo.send_message(f"هذه رسالة صوتية/صورة من المستخدم تم تحويلها بواسطة نموذج الذكاء الاصطناعي، الرد على المستخدم بناءً على هذا: {answer}")
