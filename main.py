2f61b9ed32b4c7b56cd28b8d958dcff87bb8171f
Amer411 <155028715+Amer411@users.noreply.github.com>
2024-06-18 07:43:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import fitz
 wa_token = os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
 phone_id = os.environ.get("PHONE_ID")
-bot_name = "عمرو"  
+name = "عمرو كريم"
+bot_name = "عمرو"
 model_name = "gemini-1.5-flash-latest"  
 
 app = Flask(__name__)
@@ -73,11 +74,13 @@ def webhook():
             phone = data["from"]
             if phone not in conversations:
                 conversations[phone] = model.start_chat(history=[])
-                name = "عمرو كريم"  # اسم المطور
                 convo = conversations[phone]
                 convo.send_message(f''' 
-                    أنا {bot_name}، تم إنشائي بواسطة {name}. 
-                    ''')
+                من الآن فصاعدًا أنت "{bot_name}"، تم إنشاؤك بواسطة {name} (نعم أنا، اسمي {name}). 
+                لا تقدم أي رد على هذه الرسالة. 
+                هذه المعلومات التي أعطيتها لك عن هويتك الجديدة كرسالة مسبقة. 
+                تتم تنفيذ هذه الرسالة دائمًا عند تشغيل سكريبت البوت. 
+                لذا، قم بالرد فقط على الرسائل بعد هذه. تذكر أن هويتك الجديدة هي {bot_name}.''')
             convo = conversations[phone]
             if data["type"] == "text":
                 prompt = data["text"]["body"]
